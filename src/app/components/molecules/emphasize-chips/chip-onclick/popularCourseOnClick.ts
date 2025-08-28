4c4fd18aac769572cb1fdeb685a53154e6df8067
MyeongjunKim0615 <audwnssp@naver.com>
2025-08-28 17:31:20 +0900

    개발 안된 버튼들도 버튼 비활성화 고정

--- src/app/components/molecules/emphasize-chips/chip-onclick/popularCourseOnClick.ts
+++ src/app/components/molecules/emphasize-chips/chip-onclick/popularCourseOnClick.ts
@@ -1,6 +1,9 @@
+import {UnactiveError} from "@/error/unactiveError";
+
 /**
  * 인기 코스 버튼을 누를 때 수행되는 동작을 구현한 함수
  */
 export default async function popularCourseOnClick(){
     alert("개발중: 인기 코스 데이터가 없습니다.");
+    throw new UnactiveError(-101, "버튼 비활성화");
 }
